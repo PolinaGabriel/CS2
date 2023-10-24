7c52bffa2c2469df0149ac77b10481c69a263ffb
PolinaGabriel <114815179+PolinaGabriel@users.noreply.github.com>
2023-10-24 11:15:22 +0300

    Add files via upload

---
+++ GabrielAutos/GabrielAutos/Methods.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos
+{
+	public class Avto
+	{
+		private string nom; //номер
+		private double bak; //литраж бака
+		private double ras; //расход топлива на 100 км
+		private double top; //объём бензина в баке
+		private double x1; //начальная координата x
+		private double y1; //начальная координата y
+		private double x2; //конечная координата x
+		private double y2; //конечная координата y
+		private double km; //расстояние (км)
+		private List<double> x = new(); //координаты x
+		private List<double> y = new(); //координаты y
+		private List<string> traj = new(); //траектория движения
+		private double prob; //пробег
+        public int topCheck; //проверка корректности заправки
+        public int nomCheck; //проверка совпадения номера
+        public int acc; //количество возможных аварий
+
+        /// <summary>
+        /// Выбор приватного метода
+        /// </summary>
+        /// <param name="a"></param>
+        public void Methods(int operChoice, string nom, double bak, double ras, double prob, double x1, double y1, double x2, double y2, Avto[] avtos)
+		{
+			switch (operChoice)
+			{
+					case 1:
+					{
+						InfoIn(nom, bak, ras, prob);
+                        break;
+					}
+
+					case 2:
+                    {
+                        InfoOut();
+                        break;
+                    }
+
+                    case 3:
+                    {
+                        Zapravka();
+                        break;
+
+                    }
+
+					case 4:
+                    {
+						Move(x1, y1, x2, y2);
+                        break;
+                    }
+                    
+                    case 5:
+                    {
+                        Accident(avtos, nom);
+                        break;
+                    }
+            }
+		}
+
+        /// <summary>
+        /// Ввод информации об авто
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="bak"></param>
+        /// <param name="ras"></param>
+        /// <param name="prob"></param>
+		private void InfoIn(string nom, double bak, double ras, double prob)
+		{
+			this.nom = nom;
+			this.bak = Math.Round(bak, 2);
+			this.ras = Math.Round(ras, 2);
+			this.prob = Math.Round(prob, 2);
+		}
+
+        /// <summary>
+        /// Вывод информации об авто
+        /// </summary>
+        private void InfoOut()
+        {
+            Console.WriteLine("Номер авто: " + this.nom);
+            Console.WriteLine("Литраж бензобака: " + this.bak);
+            Console.WriteLine("Расход бензина на 100 км: " + this.ras);
+            Console.WriteLine("Пробег: " + this.prob);
+            Console.WriteLine("Объём заправленного бензина: " + this.top);
+        }
+
+        /// <summary>
+        /// Заправка бака
+        /// </summary>
+        private void Zapravka()
+        {
+            Console.Write("Введите объём бензина: ");
+            double top = Math.Round(Convert.ToDouble(Console.ReadLine()), 2);
+
+            if (top <= Math.Round(this.bak - this.top, 2))
+            {
+                this.top = top;
+                this.topCheck = 1;
+            }
+        }
+
+        /// <summary>
+        /// Поездка
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+		private void Move(double x1, double y1, double x2, double y2)
+		{
+			Distance(x1, y1, x2, y2);
+
+			while (this.km > 0)
+			{
+				if (Math.Round((double)this.ras / 100 * this.km, 2) <= this.top)
+				{
+					this.prob += this.km;
+					this.top -= Math.Round((double)this.ras / 100 * this.km, 2);
+					Console.WriteLine("Вы доехали.");
+					break;
+				}
+				else
+				{
+					this.prob += Math.Round((double)this.top / this.ras * 100, 2);
+					Console.WriteLine("Вы проехали " + Math.Round((double)this.top / this.ras * 100, 2) + " км. Для дальнейшей поездки необходимо дозаправить " + Ostatok() + " л бензина. Поедете дальше?");
+					string answ = Console.ReadLine();
+
+					if (answ == "нет")
+					{
+						this.top = 0;
+						break;
+					}
+					else if (answ == "да")
+					{
+						this.km -= Math.Round((double)this.top / this.ras * 100, 2);
+						this.top = 0;
+						Zapravka();
+					}
+				}
+			}
+		}
+
+        /// <summary>
+        /// Расчёт длины пути
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        private void Distance(double x1, double y1, double x2, double y2)
+        {
+            this.x1 = Math.Round(x1, 2);
+            this.y1 = Math.Round(y1, 2);
+            this.x2 = Math.Round(x2, 2);
+            this.y2 = Math.Round(y2, 2);
+
+            this.km = Math.Round(Math.Sqrt(Math.Pow((this.x2 - this.x1), 2) + Math.Pow((this.y2 - this.y1), 2)), 2);
+        }
+
+        /// <summary>
+        /// Расчёт недостающего объёма топлива
+        /// </summary>
+        /// <returns></returns>
+        private double Ostatok()
+        {
+            return Math.Round((((double)this.ras / 100) * this.km) - this.top, 2);
+        }
+
+        /// <summary>
+        /// Расчёт возможного количества аварий
+        /// </summary>
+        /// <param name="avtos"></param>
+        /// <param name="nom"></param>
+        private void Accident(Avto[] avtos, string nom)
+        {
+            foreach (Avto avto in avtos)
+            {
+                if (avto.nom == nom)
+                {
+                    this.nomCheck = 1;
+
+                    this.Trajectory(this.x1, this.y1, this.x2, this.y2);
+                    avto.Trajectory(avto.x1, avto.y1, avto.x2, avto.y2);
+
+                    for (int i = 0; i < this.traj.Count; i++)
+                    {
+                        for (int j = 0; j < avto.traj.Count; j++)
+                        {
+                            if (this.traj[i] == avto.traj[j])
+                            {
+                                this.acc++;
+                            }
+                        }
+                    }
+
+                    this.x.Clear();
+                    avto.x.Clear();
+
+                    this.y.Clear();
+                    avto.y.Clear();
+
+                    this.traj.Clear();
+                    avto.traj.Clear();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Построение траектории движения
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        private void Trajectory(double x1, double y1, double x2, double y2)
+        {
+            this.x1 = Math.Round(x1, 2);
+            this.y1 = Math.Round(y1, 2);
+            this.x2 = Math.Round(x2, 2);
+            this.y2 = Math.Round(y2, 2);
+
+            while (this.x1 <= this.x2)
+            {
+                x.Add(this.x1);
+                this.x1++;
+            }
+
+            this.x1 = Math.Round(x1, 2);
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                y.Add(this.y1 + ((double)(this.y2 - this.y1) * (x[i] - this.x1) / (this.x2 - this.x1)));
+            }
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                this.traj.Add(Convert.ToString(x[i] + " ; " + y[i]));
+            }
+        }
+    }
+}
---
+++ GabrielAutos/GabrielAutos/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Avto[] avtos = new Avto[2];
+			avtos[0] = new Avto();
+			avtos[1] = new Avto();
+
+			string next = "";
+
+			while (next != "выход")
+			{
+				Console.WriteLine("Выберите машину:\n1\n2\n");
+				int avtoChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+				Console.WriteLine("\n");
+
+				Console.WriteLine("Выберите действие с машиной:\n1 - ввод информации об авто\n2 - вывод информации об авто\n3 - заправка бензобака\n4 - поездка\n5 - расчёт количества возможных аварий\n6 - вернуться назад\n");
+				
+				int operChoice = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("\n");
+
+				switch (operChoice)
+				{
+						case 1:
+						{
+							Console.Write("Введите номер авто: ");
+							string nom = Console.ReadLine();
+							Console.Write("Введите литраж бензобака: ");
+							double bak = Convert.ToDouble(Console.ReadLine());
+							Console.Write("Введите расход топлива на 100 км: ");
+							double ras = Convert.ToDouble(Console.ReadLine());
+							Console.Write("Введите пробег: ");
+							double prob = Convert.ToDouble(Console.ReadLine());
+							Console.WriteLine();
+							avtos[avtoChoice].Methods(operChoice, nom, bak, ras, prob, 0, 0, 0, 0, avtos);
+							Console.WriteLine("Информация внесена.");
+							break;
+						}
+
+						case 2:
+						{
+							avtos[avtoChoice].Methods(operChoice, "", 0, 0, 0, 0, 0, 0, 0, avtos);
+							break;
+						}
+
+						case 3:
+						{
+							avtos[avtoChoice].Methods(operChoice, "", 0, 0, 0, 0, 0, 0, 0, avtos);
+
+                            if (avtos[avtoChoice].topCheck == 1)
+							{
+								Console.WriteLine("Бензин заправлен.");
+							}
+							else
+							{
+								Console.WriteLine("Вы не можете заправить объём бензина, превышающий литраж бензобака. Повторите ввод.");
+							}
+
+                            avtos[avtoChoice].topCheck = 0;
+
+                            break;
+						}
+
+						case 4:
+						{
+							Console.WriteLine("Введите координаты начала пути:");
+							Console.Write("Введите x1: ");
+							double x1 = Convert.ToDouble(Console.ReadLine());
+							Console.Write("Введите y1: ");
+							double y1 = Convert.ToDouble(Console.ReadLine());
+							Console.WriteLine("Введите координаты конца пути:");
+							Console.Write("Введите x2: ");
+							double x2 = Convert.ToDouble(Console.ReadLine());
+							Console.Write("Введите y2: ");
+							double y2 = Convert.ToDouble(Console.ReadLine());
+							Console.WriteLine();
+							avtos[avtoChoice].Methods(operChoice, "", 0, 0, 0, x1, y1, x2, y2, avtos);
+							break;
+						}
+
+						case 5:
+						{
+							Console.Write("Введите номер авто, с траекторией которого хотите свериться: ");
+							string nom = Console.ReadLine();
+
+                            avtos[avtoChoice].Methods(operChoice, nom, 0, 0, 0, 0, 0, 0, 0, avtos);
+
+							if (avtos[avtoChoice].nomCheck == 1)
+							{
+								Console.WriteLine("Количество возможных аварий с участием этих машин: " + avtos[avtoChoice].acc);
+							}
+							else
+							{
+								Console.WriteLine("Номер машины для сверки введён неверно.");
+							}
+
+                            avtos[avtoChoice].nomCheck = 0;
+                            avtos[avtoChoice].acc = 0;
+
+                            break; 
+                        }
+
+						case 6:
+						{
+							break;
+                        }
+				}
+
+				Console.WriteLine("\n\nДля продолжения работы нажмите Enter.\nДля окончания введите 'выход' и нажмите Enter.\n");
+				next = Console.ReadLine();
+                Console.WriteLine("\n");
+            }
+		}
+	}
+}
